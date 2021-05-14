6463e89db654b9c0f28f455881c7ec4e0dd91a3d
theRMN <roman.vassev@gmail.com>
2021-05-14 20:11:54 +0600

    opcional task

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ import re
 
 KEYWORDS = {'дизайн', 'фото', 'web', 'python', 'toggles'}
 
-
-
-res = requests.get('https://habr.com/ru/all/')
+res = requests.get('https://habr.com/ru/all')
 
 if not res.ok:
     print('res invalid')
@@ -17,26 +15,28 @@ soup = BeautifulSoup(res.text, features='html.parser')
 for article in soup.findAll('article'):
     pattern = re.compile('([—\-\s.,:]+)')
     hubs = {h.text.lower() for h in article.findAll('a', class_='hub-link')}
-    try:
-        p_text = [set(pattern.sub(r' ', p.find('p').text.lower()).split())
-                  for p in article.findAll('div', class_='post__text_v2')][0]
-    except IndexError:
-        continue
+
+    title = article.find('h2', class_='post__title')
+    href = title.find('a').attrs.get('href')
+
+    x = requests.get(href)
+    d = BeautifulSoup(x.text, features='html.parser')
 
     if hubs & KEYWORDS:
-        title = article.find('h2', class_='post__title')
-        href = title.find('a').attrs.get('href')
         print(f'{datetime.now(tz=None)} | {title.text.strip()} | {href}')
-    elif p_text & KEYWORDS:
-        title = article.find('h2', class_='post__title')
-        href = title.find('a').attrs.get('href')
+
+    p_text = [set(pattern.sub(r' ', p.find('p').text.lower()).split())
+              for p in article.findAll('div', class_='post__text_v2')]
+
+    p_text += [set('')]
+
+    if p_text[0] & KEYWORDS:
         print(f'{datetime.now(tz=None)} | {title.text.strip()} | {href}')
-    else:
-        print('Нет совпадений')
 
+    a_text = [set(pattern.sub(r' ', a.text.lower()).split())
+              for a in d.findAll('div', class_='post__text_v1')]
 
+    a_text += [set('')]
 
-
-
-
-
+    if a_text[0] & KEYWORDS:
+        print(f'{datetime.now(tz=None)} | {title.text.strip()} | {href}')
