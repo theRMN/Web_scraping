88f5cc3807bb2f56b7fb8a715a75ef5fda59df9d
theRMN <roman.vassev@gmail.com>
2021-05-15 01:46:36 +0600

    match handling

--- main.py
+++ main.py
@@ -22,21 +22,15 @@ for article in soup.findAll('article'):
     article_link = requests.get(href)
     article_content = BeautifulSoup(article_link.text, features='html.parser')
 
-    if hubs & KEYWORDS:
-        print(f'{datetime.now(tz=None)} | {title.text.strip()} | {href}')
-
     p_text = [set(pattern.sub(r' ', p.find('p').text.lower()).split())
               for p in article.findAll('div', class_='post__text_v2')]
 
     p_text += [set('')]
-
-    if p_text[0] & KEYWORDS:
-        print(f'{datetime.now(tz=None)} | {title.text.strip()} | {href}')
 
     a_text = [set(pattern.sub(r' ', a.text.lower()).split())
               for a in article_content.findAll('div', class_='post__text_v1')]
 
     a_text += [set('')]
 
-    if a_text[0] & KEYWORDS:
+    if a_text[0] & KEYWORDS or p_text[0] & KEYWORDS or hubs & KEYWORDS :
         print(f'{datetime.now(tz=None)} | {title.text.strip()} | {href}')
